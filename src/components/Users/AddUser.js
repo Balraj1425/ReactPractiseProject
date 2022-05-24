815d3b50e1020d6310e25b8bbb8fcc8b3eb0708a
Balraj Singh Bhullar <bhullar.bs@gmail.com>
2022-05-25 01:21:41 +0530

    basic structure

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+
+const AddUser = props => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState();
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    }
+
+    const addUserHandler = (event) =>{
+        event.preventDefault();
+        console.table(enteredAge, enteredUsername)
+    };
+
+    return (
+        <Card className={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" onChange={usernameChangeHandler}/>
+                <label htmlFor="age">Age (Years)</label>
+                <input type="number" id="age" onChange={ageChangeHandler}/>
+                {/* <button type="submit">Add User</button> */}
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+    )
+}
+export default AddUser
